32964515ae5ff97b672f0d34c086caffeebceae7
Tobsi <tsh40884137@gmail.com>
2021-01-14 21:10:57 +0100

    Init Dev Branch
    Dev Branch

---
+++ cmake-build-debug/CMakeFiles/TC8E.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TC8E.dir/main.cpp.obj"
+  "TC8E.exe"
+  "TC8E.exe.manifest"
+  "TC8E.pdb"
+  "libTC8E.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TC8E.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
